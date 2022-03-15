5989f39b6a26a59639d2f16d34961065c2b42488
shalomtash <shalomtash93@gmail.com>
2022-03-15 11:34:22 +0000

    loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,11 @@
+/*
+ * File: 0-main.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
 #include "main.h"
+
 /**
   * main - Entry point
   *
@@ -6,16 +13,15 @@
   */
 int main(void)
 {
-
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
