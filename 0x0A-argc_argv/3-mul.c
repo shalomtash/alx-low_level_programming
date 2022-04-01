d982cc9930007c5212e466a64098e6bb54fd7d36
shalomtash <shalomtash93@gmail.com>
2022-04-01 14:50:15 +0000

    commit for argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,24 +2,18 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- *
- * Return: 0 if no errors, else 1
+  * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int a, b, c;
-
 	if (argc != 3)
 	{
-		puts("Error");
+		printf("Error\n");
 		return (1);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	c = a * b;
-	printf("%d\n", c);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
