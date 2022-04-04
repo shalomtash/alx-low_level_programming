bf6fb9f48be409e8a3cc295df381d6d211b4c561
shalomtash <shalomtash93@gmail.com>
2022-04-04 08:51:54 +0000

    function that splits a string

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ *
+ * Return: no return
+ */
+void ch_free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
+
+/**
  * strtow - splits a string into words.
  * @str: string.
  *
